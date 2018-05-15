3f314e89b6fea9f83fc2ddace4f59112af135864
Michał Chudecki <majk.chudecki@gmail.com>
2018-05-16 00:08:58 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('.navbar-toggle').click(function(){
+		$('.navbar-toggle').toggleClass('animate');
+		$('.navbar-collapse').toggle('slow');
+		$('.navbar-collapse').toggleClass('collapse-in');
+	});
+
+});
+
+
